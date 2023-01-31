11e416cac3d5a6968ef4660fcf712cb566890088
ryan1p1 <ryan1p1johnson@gmail.com>
2023-01-31 10:01:09 -0800

    Modified encoderreader.py

--- encoderreader.py
+++ encoderreader.py
@@ -1,3 +1,25 @@
 class Encoderreader:
     """!This class does something. Nobody knows what it does.
     """
+    def __init__(self,enca,encb,tim):
+        self.encpin1 = enca
+        self.encpin2 = encb
+        self.timer = tim
+        self.encthen = self.timer.read()
+
+    def read(self):
+        self.encnow = self.timer.read()
+        self.delt = self.encnow - self.encthen
+        self.encthen = self.encnow
+        self.position = self.position + self.delt
+        return self.position
+
+    def zero(self):
+        self.delt = self.encnow - self.encthen
+        reset = 32768
+        if self.delt > reset:
+            self.encthen = self.encnow
+            self.position = self.position - self.delt
+        elif self.delt < reset:
+            self.encthen = self.encnow
+            self.position = self.position + self.delt
