d98fead19e9cf0d567b7dd4d62680a6e5b9d590d
santiagor00 <91160149+santiagor00@users.noreply.github.com>
2023-01-31 11:00:28 -0800

    progress on encoder reader

---
+++ encoder_reader.py
@@ -0,0 +1,44 @@
+import pyb
+
+class EncoderReader:
+    """!This class does something. Nobody knows what it does.
+    """
+    def __init__(self,enca,encb,tim):
+        self.encpin1 = enca
+        self.encpin2 = encb
+        self.timer = tim
+        self.encthen = 0
+        self.position = self.encthen
+        
+        self.encnow = 0
+        
+        self.tch1 = self.timer.channel (1, pyb.Timer.ENC_AB, pin=enca)
+        self.tch2 = self.timer.channel (2, pyb.Timer.ENC_AB, pin=encb)
+
+
+    def read(self):
+        self.encnow = self.timer.counter()
+        self.delt = self.encnow - self.encthen
+        self.encthen = self.encnow
+        self.position = self.position + self.delt
+        return self.position
+
+    def zero(self):
+        
+        self.delt = self.encnow - self.encthen
+        print("encnow", self.encnow)
+        print("encthen", self.encthen)
+        print("delt", self.delt)
+        reset = 32768
+        if self.delt > reset:
+            self.encthen = self.encnow
+            self.delt -= reset
+            self.position = self.position + self.delt
+        elif self.delt < -reset:
+            self.encthen = self.encnow
+            self.delt += reset
+            self.position = self.position + self.delt
+        else:
+            self.position = self.position + self.delt
+        self.encnow = self.timer.counter()
+        return self.position
