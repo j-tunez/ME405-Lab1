8f9c4163269af88d071030f07813cace9c0ca4bf
ryan1p1 <ryan1p1johnson@gmail.com>
2023-01-31 10:42:22 -0800

    changed encoderreader.py

--- encoderreader.py
+++ encoderreader.py
@@ -7,17 +7,20 @@ class Encoderreader:
         self.encpin1 = enca
         self.encpin2 = encb
         self.timer = tim
-        self.encthen = self.timer.read()
+        self.encthen = self.timer.counter()
+
+        t8ch1 = tim.channel (1, pyb.Timer.ENC_AB, pin=enca)
+        t8ch2 = tim.channel (2, pyb.Timer.ENC_AB, pin=encb)
 
     def read(self):
-        self.encnow = self.timer.read()
+        self.encnow = self.timer.counter()
         self.delt = self.encnow - self.encthen
         self.encthen = self.encnow
         self.position = self.position + self.delt
         return self.position
 
     def zero(self):
-        self.encnow = self.timer.read()
+        self.encnow = self.timer.counter()
         self.delt = self.encnow - self.encthen
         reset = 32768
         if self.delt > reset:
